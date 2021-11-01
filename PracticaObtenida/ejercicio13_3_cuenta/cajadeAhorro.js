a626402c239c12237bf2543c586c095a9ebcf21e
unknown <bertogliofederico@hotmail.com>
2021-11-01 12:34:34 -0300

    Practica 3, cuentas y clases

--- PracticaObtenida/ejercicio13_3_cuenta/cajadeAhorro.js
+++ PracticaObtenida/ejercicio13_3_cuenta/cajadeAhorro.js
@@ -7,7 +7,10 @@ class Cuenta {
    this.sal = numero + this.sal;
  }
  debitar(numero){
-   this.sal = numero + this.sal;
+   this.sal = numero - this.sal;
+ }
+ toString(){
+   return  "<br/>numero: " + this.num + "<br/>saldo: " + this.sal;
  }
 }
 class CajaDeAhorro extends Cuenta {
@@ -15,8 +18,10 @@ class CajaDeAhorro extends Cuenta {
  super( numero, saldo );
  this.interes = interes;
  }
- debitar(numero){
-   this.sal += numero ;
-   this.interes += numero * 0.1;
+ debitar(numero ){
+   if(numero <= this.sal) this.sal -= numero ;
+ }
+ toString(){
+   return  "Caja de ahorro: " + super.toString() + "<br/>interes: " + this.interes ;
  }
 }
